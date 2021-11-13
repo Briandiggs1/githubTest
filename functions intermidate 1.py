1bc374ff51c36df18ca7d301cc86db95a7a1f496
Briandiggs1 <87153805+Briandiggs1@users.noreply.github.com>
2021-11-13 11:48:06 -0500

    functions intermediate 1

---
+++ functions intermidate 1.py
@@ -0,0 +1,26 @@
+import random
+def randInt(min= 2, max= 4):
+    num = random.random() * 100
+    return num
+#print(randInt()) 		    # should print a random integer between 0 to 100
+#print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+#print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+#print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+
+def randInt(min= 2, max= 4):
+    num = random.random() * 50
+    return num
+
+import random
+def randInt(min= 2, max= 4):
+    num = random.random() * 50 + 50
+    return num
+
+print(randInt(min=50))
+
+import random
+def randInt(min= 2, max= 4):
+    num = random.random() * 450 + 50
+    return num
+
+print(randInt(min=50, max=500))
